10f2dee0e9985b25c65be5f1a55ee25897ac3612
lee355 <66677291+lee355@users.noreply.github.com>
2024-05-20 16:43:45 +0800

    Add files via upload

---
+++ pretrain_code/fed_process.py
@@ -0,0 +1,62 @@
+# -*- coding: gbk -*-
+import numpy as np
+import os
+import sys
+import random
+import shutil
+from pathlib import Path
+
+alpha = 1.5
+client_num = 16
+random.seed(10)
+
+def split_list_by_ratio(input_list, test_ratio):
+    random.shuffle(input_list)  # 随机打乱列表中的元素顺序
+    split_index = int(len(input_list) * test_ratio)  # 根据比例计算划分的索引位置
+    test_list, train_list = input_list[:split_index], input_list[split_index:]  # 划分成两个列表
+    return train_list, test_list
+
+meta_data = np.load("/home/ly/code/src/code and baselines/dataset/imagenet2012/new_index_"+str(client_num)+"_alpha"+str(alpha)+".npy", 
+                    allow_pickle=True).tolist()
+all_image_path = Path("/home/wyy/EXP/data/ImageNet/train")
+
+print(type(meta_data))
+print(len(meta_data))
+print(meta_data.keys())
+print(meta_data[0][0])
+
+folder_path = "/home/ly/code/src/code and baselines/dataset/imagenet2012/fed_data/new_client_"+str(client_num)+"non-iid_"+str(alpha)
+
+for client_id, image_path_list in meta_data.items():
+    print("Client "+str(client_id)+" processing!!!")
+    clietn_folder = os.path.join(folder_path, 'client_'+str(client_id))
+    client_train_folder = os.path.join(clietn_folder, 'train')
+    client_test_folder = os.path.join(clietn_folder, 'test')
+    os.makedirs(clietn_folder, exist_ok=True)
+    os.makedirs(client_train_folder, exist_ok=True)
+    os.makedirs(client_test_folder, exist_ok=True)
+    label_imagesPath_dict = {}
+    for i in range(len(image_path_list)):
+        images_label = image_path_list[i].split('/')[1]
+        if images_label not in label_imagesPath_dict.keys():
+            label_imagesPath_dict[images_label] = [image_path_list[i]]
+        else:
+            label_imagesPath_dict[images_label].append(image_path_list[i])
+    #为每一个训练集和测试集建立多个图像文件夹，每一个子文件夹下文件标签相同
+    for label in label_imagesPath_dict.keys():
+        client_train_label_folder = os.path.join(client_train_folder, label)
+        client_test_label_folder = os.path.join(client_test_folder, label)
+        os.makedirs(client_train_label_folder, exist_ok=True)
+        os.makedirs(client_test_label_folder, exist_ok=True)
+    #开始转移图片
+    for label, image_path_list in label_imagesPath_dict.items(): 
+        train_list, test_list = split_list_by_ratio(label_imagesPath_dict[label], test_ratio=0.2)
+        train_destination_folder = os.path.join(client_train_folder, label)
+        test_destination_folder = os.path.join(client_test_folder, label)
+        for image_path in train_list:
+            image_path = all_image_path.joinpath(image_path)
+            shutil.copy(image_path, train_destination_folder)
+        for image_path in test_list:
+            image_path = all_image_path.joinpath(image_path)
+            shutil.copy(image_path, test_destination_folder)
+        
---
+++ pretrain_code/transfer_data.py
@@ -0,0 +1,60 @@
+# -*- coding: gbk -*-
+import paramiko
+import os
+import subprocess
+import pexpect
+
+# 本地文件夹路径
+local_folder = '/home/ly/code/src/code and baselines/dataset/imagenet2012/fed_data/new_client_16non-iid_1.5'
+
+# 远程服务器信息
+servers = [
+    {'hostname': '10.102.32.203', 'username': 'xugw', 'password': 'iic123456', 'remote_folder': '/home/xugw/fed_data/'},
+    {'hostname': '10.102.32.203', 'username': 'xugw', 'password': 'iic123456', 'remote_folder': '/home/xugw/fed_data/'},
+    {'hostname': '101.76.210.28', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.219.170', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.220.29', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.214.45', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.212.157', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.210.134', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.215.73', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.211.61', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.220.69', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.218.162', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.212.180', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.216.237', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.211.239', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'},
+    {'hostname': '101.76.220.159', 'username': 'nvidia', 'password': 'nvidia', 'remote_folder': '/home/nvidia/fed_data/'}
+]
+
+# 遍历每个远程服务器
+for i in range(len(servers)):
+    print("Start transferring client" + str(i))
+    servers[i]['remote_folder'] += 'new_client_16non-iid_1.5'
+    sub_local_folder = local_folder + "/client_" + str(i)
+    # 建立SSH连接
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(servers[i]['hostname'], username=servers[i]['username'], password=servers[i]['password'])
+
+    # 创建SFTP客户端
+    sftp = ssh.open_sftp()
+
+    try:
+        sftp.chdir(servers[i]['remote_folder'])
+    except IOError:
+        sftp.mkdir(servers[i]['remote_folder'])
+        
+    # 递归传输子文件夹
+    for root, dirs, files in os.walk(sub_local_folder):
+        remote_dir = os.path.join(servers[i]['remote_folder'], os.path.relpath(root, local_folder))
+        try:
+            sftp.mkdir(remote_dir)
+        except IOError:
+            pass
+        for file in files:
+            sftp.put(os.path.join(root, file), os.path.join(remote_dir, file))
+
+    # 关闭SFTP客户端和SSH连接
+    sftp.close()
+    ssh.close()
